0bea926f3fb7878e698d99697fffafab4b55eb80
Yashvardhan Jagnani <jagnani73@users.noreply.github.com>
2020-09-18 22:26:27 +0530

    feat(client): /contact-us responsive

--- client/src/components/ContactUs/Hero/Hero.tsx
+++ client/src/components/ContactUs/Hero/Hero.tsx
@@ -11,8 +11,8 @@ import {
 const Hero = () => {
   return (
     <div className="flex flex-wrap text-5xl w-screen h-screen bg-contactUsBG bg-center bg-no-repeat bg-cover">
-      <div className="w-1/2 m-auto text-white text-center">
-        <div className="w-1/2 ml-auto my-auto bg-contactUs shadow-contactUs text-base font-bold p-10">
+      <div className="w-3/4 lg:w-1/2 m-auto text-white text-center">
+        <div className="w-full lg:w-1/2 ml-auto my-auto bg-contactUs shadow-contactUs text-base font-bold p-10">
           <h1>Aaruush Room, opposite Placement Cell </h1>
           <h1>SRM IST, Kattankulathur, TN - 603203</h1>
           <h2 className="mt-4">
@@ -24,12 +24,12 @@ const Hero = () => {
           <h2 className="mt-4">
             <a href="mailto:csiorg@aaruush.net">csiorg@aaruush.net</a>
           </h2>
-          <div className="flex flex-wrap text-3xl w-full mx-auto mt-6">
+          <div className="flex flex-wrap text-xl lg:text-2xl w-full mx-auto mt-6">
             <a
               href="https://twitter.com/aaruushsrmist"
               target="_blank"
               rel="noopener noreferrer"
-              className="flex flex-wrap w-12 h-12 mx-auto bg-twitter rounded-md"
+              className="flex flex-wrap w-8 md:w-10 h-8 md:h-10 mx-auto bg-twitter rounded-md"
             >
               <FontAwesomeIcon icon={faTwitter} className="m-auto" />
             </a>
@@ -37,7 +37,7 @@ const Hero = () => {
               href="https://www.instagram.com/aaruush_srm"
               target="_blank"
               rel="noopener noreferrer"
-              className="flex flex-wrap w-12 h-12 mx-auto bg-instagram rounded-md"
+              className="flex flex-wrap w-8 md:w-10 h-8 md:h-10 mx-auto bg-instagram rounded-md"
             >
               <FontAwesomeIcon icon={faInstagram} className="m-auto" />
             </a>
@@ -45,7 +45,7 @@ const Hero = () => {
               href="https://www.facebook.com/aaruush.srm"
               target="_blank"
               rel="noopener noreferrer"
-              className="flex flex-wrap w-12 h-12 mx-auto bg-facebook rounded-md"
+              className="flex flex-wrap w-8 md:w-10 h-8 md:h-10 mx-auto bg-facebook rounded-md"
             >
               <FontAwesomeIcon icon={faFacebookF} className="m-auto" />
             </a>
@@ -53,7 +53,7 @@ const Hero = () => {
               href="https://www.youtube.com/channel/UC6mwWpwkZchii-oyWz0v3dw"
               target="_blank"
               rel="noopener noreferrer"
-              className="flex flex-wrap w-12 h-12 mx-auto bg-youtube rounded-md"
+              className="flex flex-wrap w-8 md:w-10 h-8 md:h-10 mx-auto bg-youtube rounded-md"
             >
               <FontAwesomeIcon icon={faYoutube} className="m-auto" />
             </a>
@@ -61,7 +61,7 @@ const Hero = () => {
               href="https://www.linkedin.com/company/aaruush-srm-ist/mycompany/"
               target="_blank"
               rel="noopener noreferrer"
-              className="flex flex-wrap w-12 h-12 mx-auto bg-linkedin rounded-md"
+              className="flex flex-wrap w-8 md:w-10 h-8 md:h-10 mx-auto bg-linkedin rounded-md"
             >
               <FontAwesomeIcon icon={faLinkedinIn} className="m-auto" />
             </a>
