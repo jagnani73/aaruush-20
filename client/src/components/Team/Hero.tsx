ab46bd50e8eaaa9f146d2e746655f7ecd753a280
Yashvardhan Jagnani <jagnani73@users.noreply.github.com>
2020-09-19 19:31:59 +0530

    feat(client): /team responsive

--- client/src/components/Team/Hero.tsx
+++ client/src/components/Team/Hero.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import teamMember from "../../assets/Team/teamMember.jpg";
 import Member from "./Member";
 
-const Team = () => {
+const Hero = () => {
   const Members: { name: string; designation: string; img: string }[] = [
     { name: "Andy Richter", designation: "Conan's PA", img: teamMember },
     { name: "Andy Richter", designation: "Conan's PA", img: teamMember },
@@ -15,10 +15,10 @@ const Team = () => {
 
   return (
     <div className="flex flex-wrap w-full">
-      <div className="flex flex-wrap w-1/2">
+      <div className="flex flex-wrap w-full lg:w-1/2">
         <div className="w-10/12 mx-auto mt-10">
-          <h1 className="text-4xl">Meet the Team</h1>
-          <p className="w-1/2 my-10">
+          <h1 className="text-4xl text-justify lg:text-left">Meet the Team</h1>
+          <p className="w-full lg:w-1/2 my-10">
             Lorem qui tempor nostrud adipisicing sint mollit aute laboris duis
             eu culpa mollit sint. Eiusmod adipisicing qui incididunt cupidatat
             officia pariatur ex dolor do eiusmod cillum. Tempor irure elit
@@ -28,7 +28,7 @@ const Team = () => {
       </div>
       <div className="flex flex-wrap w-full">
         {Members.map((member, index) => (
-          <div key={index} className="w-1/6 mx-auto px-10">
+          <div key={index} className="w-1/2 lg:w-1/6 mx-auto px-3 lg:px-6 mt-5">
             <Member
               name={member.name}
               designation={member.designation}
@@ -41,4 +41,4 @@ const Team = () => {
   );
 };
 
-export default Team;
+export default Hero;
--- client/src/pages/Team.tsx
+++ client/src/pages/Team.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Hero from "../components/Team/Team";
+import Hero from "../components/Team/Hero";
 
 const Team = () => {
   return (
