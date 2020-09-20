5846cfd2f1aebe4057ec6b3541dd393857c072ee
Ankit Hans <ankithans1947@gmail.com>
2020-09-20 13:12:09 +0530

    feat(server): deploy scripts

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ require('dotenv').config();
 
 const teamRoute = require('./routes/team');
 
+if (process.env.NODE_ENV === 'maintainance') {
+  app.use('*', (req, res) => res.sendStatus(503));
+}
 // Connect Database
 connectDB();
 
@@ -21,12 +24,24 @@ app.use('/api/v1', teamRoute);
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  );
+  app.get('*', (req, res) => {
+    try {
+      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  });
 }
+
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    error: `Cannot ${req.method} ${req.url}`,
+  });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
