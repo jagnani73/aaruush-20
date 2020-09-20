6727b007806d2d6bc4235d0c4d57e4ed01b1b2af
Ankit Hans <ankithans1947@gmail.com>
2020-09-20 12:37:39 +0530

    chor(server): prepare for deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./database/db');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 require('dotenv').config();
 
@@ -16,6 +17,16 @@ app.use(express.json({ extended: false }));
 
 // Define Routes
 app.use('/api/v1', teamRoute);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
